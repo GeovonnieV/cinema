e7a5fec3bf0d746ecc3cd774983ea29233502104
unknown <geovonnnievasquez@gmail.com>
2021-06-02 13:27:23 -0500

    dark overlay pic

--- cinema/src/components/Header.jsx
+++ cinema/src/components/Header.jsx
@@ -1,16 +1,31 @@
 import React from "react";
-import { Jumbotron, Container } from "react-bootstrap";
+import {
+  Jumbotron,
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 
 const Header = () => {
   return (
     <div className="container">
       <Jumbotron fluid className="header-jumbo-div">
         <Container>
-          <h1>Fluid jumbotron</h1>
-          <p>
-            This is a modified jumbotron that occupies the entire horizontal
-            space of its parent.
-          </p>
+            <h1 className="display-1">Welcome.</h1>
+            <p>Find upcoming, new, and past movies</p>
+            <div className="header-input">
+              <InputGroup className="mb-3 whata">
+                <FormControl
+                  placeholder="Search for a movie.."
+                  aria-label="Recipient's username"
+                  aria-describedby="basic-addon2"
+                />
+                <InputGroup.Append>
+                  <Button variant="primary">Search</Button>
+                </InputGroup.Append>
+              </InputGroup>
+            </div>
         </Container>
       </Jumbotron>
     </div>
