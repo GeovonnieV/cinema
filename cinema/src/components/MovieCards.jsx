abd3bb460ad11b3f9a5e5a35313270b0d894f761
unknown <geovonnnievasquez@gmail.com>
2021-06-05 09:43:59 -0500

    opactiy cards hover

--- cinema/src/components/MovieCards.jsx
+++ cinema/src/components/MovieCards.jsx
@@ -1,32 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-const MovieCards = ({nowPlaying}) => {
+const MovieCards = ({ nowPlaying }) => {
   const cardImageBaseUrl = "https://image.tmdb.org/t/p/w500";
+  const movieOne = nowPlaying[0];
+  const movieTwo = nowPlaying[1];
+  const movieThree = nowPlaying[2];
+  const movieFour = nowPlaying[3];
 
   return (
     <section className="card-container">
       <div className="movie-card">
-        <img src={cardImageBaseUrl + nowPlaying[0].poster_path} className="card-image" />
+        <img
+          src={cardImageBaseUrl + movieOne.poster_path}
+          className="card-image"
+        />
         <div className="movie-card-text">
-          <h3 style={{fontWeight: "lighter"}}>{nowPlaying[0].title}</h3>
+          <h3 style={{ fontWeight: "lighter" }}>{movieOne.title}</h3>
         </div>
       </div>
       <div className="movie-card">
-        <img src={cardImageBaseUrl + nowPlaying[0].poster_path} className="card-image" />
+        <img
+          src={cardImageBaseUrl + movieTwo.poster_path}
+          className="card-image"
+        />
         <div className="movie-card-text">
-          <h3 style={{fontWeight: "lighter"}}>{nowPlaying[0].title}</h3>
+          <h3 style={{ fontWeight: "lighter" }}>{movieTwo.title}</h3>
         </div>
       </div>
       <div className="movie-card">
-        <img src={cardImageBaseUrl + nowPlaying[0].poster_path} className="card-image" />
+        <img
+          src={cardImageBaseUrl + movieThree.poster_path}
+          className="card-image"
+        />
         <div className="movie-card-text">
-          <h3 style={{fontWeight: "lighter"}}>{nowPlaying[0].title}</h3>
+          <h3 style={{ fontWeight: "lighter" }}>{movieThree.title}</h3>
         </div>
       </div>
       <div className="movie-card">
-        <img src={cardImageBaseUrl + nowPlaying[0].poster_path} className="card-image" />
+        <img
+          src={cardImageBaseUrl + movieFour.poster_path}
+          className="card-image"
+        />
         <div className="movie-card-text">
-          <h3 style={{fontWeight: "lighter"}}>{nowPlaying[0].title}</h3>
+          <h3 style={{ fontWeight: "lighter" }}>{movieFour.title}</h3>
         </div>
       </div>
     </section>
