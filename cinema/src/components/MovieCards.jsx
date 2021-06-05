fdbadfae39c21c97cfb095937059af79007fc06f
unknown <geovonnnievasquez@gmail.com>
2021-06-05 08:12:54 -0500

    flexbox

--- cinema/src/components/MovieCards.jsx
+++ cinema/src/components/MovieCards.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 
 const MovieCards = ({nowPlaying}) => {
+  const cardImageBaseUrl = "https://image.tmdb.org/t/p/w500";
+
   return (
     <section className="card-container">
       <div className="movie-card">
-        <img  className="card-image" />
+        <img src={cardImageBaseUrl + nowPlaying[0].poster_path} className="card-image" />
         <div className="movie-card-text">
-          <h4 style={{fontWeight: "lighter"}}>{nowPlaying[0].title}</h4>
+          <h3 style={{fontWeight: "lighter"}}>{nowPlaying[0].title}</h3>
+        </div>
+      </div>
+      <div className="movie-card">
+        <img src={cardImageBaseUrl + nowPlaying[0].poster_path} className="card-image" />
+        <div className="movie-card-text">
+          <h3 style={{fontWeight: "lighter"}}>{nowPlaying[0].title}</h3>
+        </div>
+      </div>
+      <div className="movie-card">
+        <img src={cardImageBaseUrl + nowPlaying[0].poster_path} className="card-image" />
+        <div className="movie-card-text">
+          <h3 style={{fontWeight: "lighter"}}>{nowPlaying[0].title}</h3>
+        </div>
+      </div>
+      <div className="movie-card">
+        <img src={cardImageBaseUrl + nowPlaying[0].poster_path} className="card-image" />
+        <div className="movie-card-text">
+          <h3 style={{fontWeight: "lighter"}}>{nowPlaying[0].title}</h3>
         </div>
       </div>
     </section>
